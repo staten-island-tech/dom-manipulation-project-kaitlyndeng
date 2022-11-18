42fce75060a045844db38294f6d224d037f4e745
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-11-18 10:16:18 -0500

    DOM Commit 9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DOMSelectors = {
   button: document.querySelector(".btn"),
-  box: document.getElementById("box"),
+  card: document.getElementById("box"),
   top: document.getElementById("top"),
   url: document.getElementById("url"),
   btm: document.getElementById("btm"),
@@ -10,15 +10,23 @@ const DOMSelectors = {
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const info = {
-  top: DOMSelectors.top.value,
-  url: DOMSelectors.url.value,
-  btm: DOMSelectors.btm.value,
-  });
-  //create object from values
-  //only use 1 insertadjcent html
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${info} </p>`);
-  DOMSelectors.info.value = "";
+  let top = DOMSelectors.top.value;
+  console.log(top);
+  let url = DOMSelectors.url.value;
+  console.log(top);
+  let btm = DOMSelectors.btm.value;
+  console.log(top);
+  DOMSelectors.card.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+<h2 id="top-text">${top}</h2>
+<img id="img" src= "${url}"/>
+<h3 id="bottom-text">${btm}</h3>
+<button id="remove-card">Remove</button>
+</div>`
+  );
+});
+
 //   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${url} </p>`);
 //   DOMSelectors.url.value = "";
 //   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${btm} </p>`);
