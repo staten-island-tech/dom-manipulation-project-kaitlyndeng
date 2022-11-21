631374dbfa95e10875bda65f5ddd56d8a2d19c35
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-11-21 01:05:53 -0500

    Commit 11

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DOMSelectors = {
   button: document.querySelector(".btn"),
-  card: document.getElementById("box"),
+  box: document.getElementById("box"),
   top: document.getElementById("top"),
   url: document.getElementById("url"),
   btm: document.getElementById("btm"),
@@ -11,40 +11,25 @@ const DOMSelectors = {
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   let top = DOMSelectors.top.value;
-  console.log(top);
   let url = DOMSelectors.url.value;
-  console.log(top);
   let btm = DOMSelectors.btm.value;
-  console.log(top);
-  DOMSelectors.card.insertAdjacentHTML(
+  DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
 <h2 id="top-text">${top}</h2>
-<img id="img" src= "${url}"/>
+<img id="img" src = "${url}">
 <h3 id="bottom-text">${btm}</h3>
-<button id="remove-card">Remove</button>
+<button type = "button" class="remove" onclick="remove>Remove</button>
 </div>`
   );
 });
+function clear () {
+DOMSelectors.top.value = "";
+DOMSelectors.url.value = "";
+DOMSelectors.btm.value = "";
+}
 
-//   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${url} </p>`);
-//   DOMSelectors.url.value = "";
-//   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${btm} </p>`);
-//   DOMSelectors.btm.value = "";
-// });
-
-// const DOMSelectors = {
-//   top: document.getElementById("top"),
-//   url: document.getElementById("url"),
-//   bottom: document.getElementById("bottom"),
-//   button: document.querySelector(".btn"),
-//   memeify: document.getElementById("form"),
-//   section: document.getElementById("display"),
-// };
-
-// DOMSelectors.memeify.addEventListener("form", function (e) {
-//   e.preventDefault();
-//   console.log(e);
-// });
-
-// values;
+function remove() {
+  const card = document.getElementById(`card`);
+  card.remove();
+}
