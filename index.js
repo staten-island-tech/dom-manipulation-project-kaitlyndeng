ba4d1c55f89b657ac1cd6c992aa3dd58861d3d8b
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-11-27 16:43:50 -0500

    DOM Commit 13

--- index.js
+++ index.js
@@ -6,39 +6,41 @@ const DOMSelectors = {
   btm: document.getElementById("btm"),
   form: document.getElementById("form"),
 };
-// console.log(DOMSelectors.box);
-function remove(event) {
-  console.log(event.target);
-  event.target.remove();
-}
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  let top = DOMSelectors.top.value;
-  let url = DOMSelectors.url.value;
-  let btm = DOMSelectors.btm.value;
+  console.log(event);
+  function card (){
+    let top = DOMSelectors.top.value;
+    let url = DOMSelectors.url.value;
+    let btm = DOMSelectors.btm.value;
+
+  function insert (){
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-<h2 id="top-text">${top}</h2>
-<img id="img" src = "${url}">
-<h3 id="bottom-text">${btm}</h3>
-<button type= "button" class="button">Remove</button>
-</div>`
-  );
-});
-
-function clear() {
-  DOMSelectors.top.value = "";
-  DOMSelectors.url.value = "";
-  DOMSelectors.btm.value = "";
+    <h2 id="top-text">${top}</h2>
+    <img id="img" src = "${url}">
+    <h3 id="bottom-text">${btm}</h3>
+    <button type= "button" class="button">Remove</button>
+    </div>`
+)};
+ 
+insert()
 }
+card();
 
-function remove() {
-  const remove = document.querySelectorAll(`.button`);
+const remove = document.querySelectorAll(`.button`);
   remove.forEach((button) => {
     button.addEventListener("click", function (e) {
       e.target.parentNode.remove();
     });
   });
+  
+function clear() {
+  DOMSelectors.top.value = "";
+  DOMSelectors.url.value = "";
+  DOMSelectors.btm.value = "";
 }
+clear (); 
+})
